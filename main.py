1888f8f64f819a590583483ae37398d664bf3987
repl.it user <replituser@example.com>
2021-05-31 19:17:57 +0000

    last update

--- main.py
+++ main.py
@@ -1,61 +1,20 @@
-class Person:
+from inhabitant import Inhabitant
 
-  def __init__(self, fn, ln):
-    self.first_name = fn
-    self.last_name = ln
-    self.address = None
-
-  def set_address(self, address):
-    self.address = address
+class Alien(Inhabitant):
   
-  def __str__(self):
-    return f"{self.first_name} {self.last_name}"
+  def __init__(self, name = "ET", age = 0):
+
+    self.technology = []
+    super().__init__(name, age)
+
+  def pickup(self, tech):
+    self.technology.append(tech)
+
+  def drop(self, tech):
+    self.technology.remove(tech)
 
 
-class BankAccount:
+if __name__ == "__main__":
+  et = Alien()
 
-  def __init__(self, sort_code, account_number):
-    self.sort_code = sort_code
-    self.account_number = account_number
-    self.balance = 0
-
-  def pay_in(self, money):
-    self.balance += money
-
-  def withdraw(self, money):
-    self.balance -= money
-
-
-
-class IndividualAccount(BankAccount):
-
-  def __init__(self, sort_code, account_number, owner):
-    super().__init__(sort_code, account_number)
-    if type(owner) is Person:
-      self.owner = owner
-    else:
-      self.owner = None
-
-  def get_account_data(self):
-    print(f"The account belongs to {self.owner}. The account number is {self.account_number}. The balance is {self.balance}")
-
-
-
-class SharedAccount(BankAccount):
-
-  def __init__(self, sort_code, account_number ):
-    super().__init__(sort_code, account_number)
-    self.owners = []
-
-  def get_account_data(self):
-    full_owners =""
-    for person in self.owners:
-      full_owners + person + " "
-    print(f"This shared account belongs to {full_owners}. The account number is {self.account_number}. The balance is {self.balance}")
-
-
-p1 = Person("Piotr", "Bednorz")
-
-acc1 = IndividualAccount("13343", "232424", p1)
-
-acc1.get_account_data()
+  print(et)
